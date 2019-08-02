753c49be33d91afadf639a856cb83139b63885fe
Steve Biedermann <steve.biedermann@mquadr.at>
2019-08-02 13:18:29 +0200

    (c) use threads to send messages (c) allow configuration via commandline args (+) logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use std::sync::{Arc, Mutex};
+#[macro_use]
+extern crate log;
 
+use std::sync::{Arc, Mutex, RwLock};
+
+use argparse::{ArgumentParser, StoreOption};
 use riker::actors::*;
 
 struct Collector {
@@ -11,6 +15,7 @@ struct Collector {
 impl Collector {
     fn actor(args: (Arc<Mutex<std::sync::mpsc::Sender<()>>>, usize)) -> Self {
         let (cv, amount) = args;
+        info!("Collector started, waiting for {} messages", amount);
         Collector {
             cv,
             amount,
@@ -19,7 +24,7 @@ impl Collector {
     }
 
     fn props(cv: Arc<Mutex<std::sync::mpsc::Sender<()>>>, amount: usize) -> BoxActorProd<Collector> {
-        Props::new_args(Box::new(Collector::actor), (cv, amount))
+        Props::new_args(Collector::actor, (cv, amount))
     }
 }
 
@@ -51,43 +56,94 @@ impl Actor for MyActor {
             ctx: &Context<String>,
             msg: String,
             _sender: Sender) {
-        println!("[{}] :: Received: {}", ctx.myself.name(), msg);
+        debug!("[{}] :: Received: {}", ctx.myself.name(), msg);
         self.collector.tell((), None);
     }
 }
 
 impl MyActor {
-    fn actor(collector: ActorRef<()>) -> Self {
+    fn actor(args: (usize, ActorRef<()>)) -> Self {
+        let (index, collector) = args;
+        info!("Actor({}) Started", index);
         MyActor {
             collector
         }
     }
 
-    fn props(collector: ActorRef<()>) -> BoxActorProd<MyActor> {
-        Props::new_args(Box::new(MyActor::actor), collector)
+    fn props(args: (usize, ActorRef<()>)) -> BoxActorProd<MyActor> {
+        Props::new_args(MyActor::actor, args)
     }
 }
 
-const ACTOR_COUNT: usize = 40;
-const MESSAGE_COUNT: usize = 40000;
+struct Options {
+    actor_count: usize,
+    message_count: usize,
+}
+
+fn get_options() -> Options {
+    let mut actor_count = None;
+    let mut message_count = None;
+    {
+        // this block limits scope of borrows by ap.refer() method
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Run riker test.");
+        ap.refer(&mut actor_count)
+            .add_option(&["-a", "--actor-count"], StoreOption, "how many actors to start");
+        ap.refer(&mut message_count)
+            .add_option(&["-m", "--message-count"], StoreOption, "how many messages to send");
+        ap.parse_args_or_exit();
+    }
+
+    let actor_count = actor_count.unwrap_or(num_cpus::get_physical());
+    Options {
+        actor_count,
+        message_count: message_count.unwrap_or(actor_count * 1000),
+    }
+}
 
 // start the system and create an actor
 fn main() {
+    env_logger::init();
+
+    let options = get_options();
+
     let sys = ActorSystem::new().unwrap();
 
+    let sender_count = num_cpus::get_physical();
+
     let (tx, rx) = std::sync::mpsc::channel();
-    let amount = MESSAGE_COUNT;
+    let amount = options.message_count - (options.message_count % sender_count);
+    let actor_count = options.actor_count;
 
-    let collector = sys.actor_of(Collector::props(Arc::new(Mutex::new(tx)), amount), "collector").unwrap();
+    info!("Message count: {}", amount);
 
-    let actors = (0..ACTOR_COUNT).map(|i| sys.actor_of(MyActor::props(collector.clone()), &format!("my-actor-{}", i)).unwrap()).collect::<Vec<_>>();
+    info!("Starting Collector");
+    let collector = sys.actor_of(Collector::props(Arc::new(Mutex::new(tx.clone())), amount), "collector").unwrap();
 
-    for i in 0..MESSAGE_COUNT {
-        actors.get(i % ACTOR_COUNT).unwrap().tell("Hello my actor!".to_string(), None);
+    info!("Starting {} Actors", actor_count);
+    let actors = (0..actor_count).map(|i| sys.actor_of(MyActor::props((i, collector.clone())), &format!("my-actor-{}", i)).unwrap()).collect::<Vec<_>>();
+
+    let actors = Arc::new(RwLock::new(actors));
+
+    let amount_per_thread = amount / sender_count;
+
+    info!("Starting {} Sender Threads", sender_count);
+    for c in 0..sender_count {
+        let actors_clone = actors.clone();
+        std::thread::spawn(move || {
+            for i in c..amount_per_thread + c {
+                let actor = actors_clone.read().unwrap();
+                let index = i % actor_count;
+                debug!("Sender({}) sending Message to Actor({})", c, index);
+                actor.get(index).unwrap().tell(format!("Hello from Sender({})", c), None);
+            }
+        });
     }
 
+    info!("Waiting for all actors to finish");
     let _ = rx.recv();
 
+    info!("Shutting down");
     let mut r = sys.shutdown();
     loop {
         if let Ok(Some(())) = r.try_recv() {
